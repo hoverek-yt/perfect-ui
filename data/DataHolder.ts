1f21c20b40641c80d49a8c74dbddb4e3c096a19b
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-21 19:00:23 +0100

    .

--- data/DataHolder.ts
+++ data/DataHolder.ts
@@ -1,5 +1,7 @@
-export abstract class DataHolder {
-  private _parent: DataHolder;
+import { Emitter } from '../lifecycle/Emitter'
+
+export abstract class DataHolder extends Emitter {
+  private _parent?: DataHolder;
   public get parent() {
     return this._parent;
   }
