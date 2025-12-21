0c7ad57e6b3bcf4f8f6d1a6a3a13d8154ddeadc4
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-21 18:56:25 +0100

    .

--- lifecycle/Emitter.ts
+++ lifecycle/Emitter.ts
@@ -1,3 +1,34 @@
+export type EmitterEventHandler = () => void;
+
 export abstract class Emitter {
-  private _eventMap: Map<string, string> = new Map();
+  private _eventMap: Map<string, EmitterEventHandler[]> = new Map();
+  public get eventMap() {
+    return this._eventMap;
+  }
+
+  private ensureEventArrayExists(event: string) {
+    if (!this.eventMap.has(event)) {
+      this.eventMap.set(event, []);
+    }
+  }
+
+  public on(event: string, handler: EmitterEventHandler) {
+    this.ensureEventArrayExists(event);
+
+    this.eventMap.get(event).push(handler);
+    return this;
+  }
+
+  public off(event: string, handler: EmitterEventHandler) {
+    const handlers = this.eventMap.get(event);
+    const handlerIndex = handlers.indexOf(handler);
+
+    if (handlerIndex > 0) {
+      handlers.splice(handlerIndex, 1);
+
+      return true;
+    }
+
+    return false;
+  }
 }
