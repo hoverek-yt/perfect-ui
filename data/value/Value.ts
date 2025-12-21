bce7748cae1b3616a982d4c6d7badfa6db248bc0
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-21 18:20:13 +0100

    .

--- data/value/Value.ts
+++ data/value/Value.ts
@@ -1,11 +1,11 @@
 import { DataHolder } from "../DataHolder";
 
 export abstract class Value<T> extends DataHolder {
-  private _value: T
+  private _value: T;
   public set value(value: T) {
     this._value = value;
   }
   public get value() {
-    return this._value
+    return this._value;
   }
 }
--- ui/component/Component.ts
+++ ui/component/Component.ts
@@ -1,10 +1,10 @@
 export class Component<E extends HTMLElement> {
-	private _nativeElement: E
+	private _nativeElement: E;
 	public get nativeElement() {
-		return this._nativeElement
+		return this._nativeElement;
 	}
 
 	constructor(element: E) {
-		this._nativeElement = element
+		this._nativeElement = element;
 	}
 }
