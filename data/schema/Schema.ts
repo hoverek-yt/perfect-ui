8dc9279ba92cf73f21751245d8560f14e8636746
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-21 20:05:11 +0100

    .

--- data/schema/Schema.ts
+++ data/schema/Schema.ts
@@ -1,7 +1,7 @@
 import { Field } from "../field/Field";
 
 export class Schema<T extends Record<string, Field>> {
-  private _fields: T = {};
+  private _fields: T;
 
   constructor(fields: T) {
     this._fields = fields;
