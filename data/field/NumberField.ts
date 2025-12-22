28a083e3009a4eff7cf12699548426197b0e0474
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-22 12:08:43 +0100

    .

--- data/field/NumberField.ts
+++ data/field/NumberField.ts
@@ -11,7 +11,7 @@ export class NumberField extends Field {
     Object.apply(this, props);
   }
 
-  validate(data: any): boolean {
+  isValid(data: any): boolean {
     const min = Math.min(...this.range);
     const max = Math.max(...this.range);
 
