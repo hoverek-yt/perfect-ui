79da02b5d6734bfec61508a926211898a651e058
hoverek-yt <89012686+hoverek-yt@users.noreply.github.com>
2025-12-23 12:54:07 +0100

    .

--- ui/component/layout/Layout.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from "../Component";
-
-export abstract class Layout extends Component<HTMLElement> { }
--- ui/style/Style.ts
+++
@@ -1,3 +0,0 @@
-import { Emitter, EventMap } from "../../lifecycle/Emitter";
-
-export class Style<EM extends EventMap = object> extends Emitter<EM> { }
